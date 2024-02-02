8e640e1a714b536fe0952fa95cf115da5f2b5f57
bryancsinformatica <bryancsinformatica@gmail.com>
2024-02-02 11:00:06 -0600

    fix some bugs

--- ContainsDuplicate/Solution.java
+++ ContainsDuplicate/Solution.java
@@ -3,15 +3,15 @@ package ContainsDuplicate;
 import java.util.HashSet;
 
 public class Solution {
-    //| RunTime : 8 ms | Memory: 55 MB
-    public boolean containsDuplicate(int[] nums){
+    // | RunTime : 8 ms | Memory: 55 MB
+    public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> numbers = new HashSet<>();
-        for(int num: nums){
-            if(!numbers.add(num)){
-               System.gc();
-               return true;
+        for (int num : nums) {
+            if (!numbers.add(num)) {
+                System.gc();
+                return true;
             }
         }
-        return false; 
-    }e
+        return false;
+    }
 }
