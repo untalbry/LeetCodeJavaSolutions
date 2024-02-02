863d6048e605b80944dee0b00c6f37c9d849b490
bryancsinformatica <bryancsinformatica@gmail.com>
2024-02-02 12:28:02 -0600

    add new solution for TwoSum and starting his Solution Explanation

--- TwoSum/Solution.java
+++ TwoSum/Solution.java
@@ -1,16 +1,25 @@
 package TwoSum;
 
 public class Solution {
-    //| RunTime : 45 ms | Memory: 43.4 MB
+    // | RunTime : 0 ms | Memory: 44.45MB
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
+        int leftPointer = 0;
+        int rightPointer = nums.length - 1;
+        for (; leftPointer < nums.length / 2; leftPointer++) {
+            if (nums[leftPointer] + nums[rightPointer] == target) {
+                return new int[] { leftPointer, rightPointer };
+            }
+            for (int j = leftPointer + 1; j < rightPointer; j++) {
+                if (nums[leftPointer] + nums[j] == target) {
+                    return new int[] { leftPointer, j };
+                }
+                if (nums[j] + nums[rightPointer] == target) {
+                    return new int[] { j, rightPointer };
                 }
             }
+            rightPointer--;
         }
-        return new int[0];
+        return new int[] { 0, 0 };
 
     }
 }
---
+++ TwoSum/SolutionExplanation.md
@@ -0,0 +1,3 @@
+# Two Sum
+
+The `Two sum` problem its a problem that consist in find the two numbers sush that they add up to the `target` 
