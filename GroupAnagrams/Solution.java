ca3028022dbaca07f9775ef152ba042af84f636c
bryancsinformatica <bryancsinformatica@gmail.com>
2024-02-01 19:06:51 -0600

    add description for Longest Consecutive Sequence

--- GroupAnagrams/Solution.java
+++ GroupAnagrams/Solution.java
@@ -5,12 +5,11 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Solution {
     // Runtime: 6 ms | Memory: 47.83 MB
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> group = new HashMap();
+        Map<String, List<String>> group = new HashMap<>();
         for(String str: strs){
             String base = anagram(str);
             if(!group.containsKey(base)) group.put(base, new ArrayList<>());
