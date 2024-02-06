b4951374e28cb376ac604e9856f7c50cd4755572
bryancsinformatica <bryancsinformatica@gmail.com>
2024-02-06 15:22:48 -0600

    Valid palindrome problem

---
+++ ValidPalindrome/Solution.java
@@ -0,0 +1,27 @@
+package ValidPalindrome;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("0P"));
+    }
+      // | RunTime : 3 ms | Memory: 42.84 MB
+    public static boolean isPalindrome(String s){
+        if(s == null || s.isEmpty()) return true; 
+        s = s.toLowerCase(); 
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left)))
+                left++;
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right)))
+                right--;
+            if(s.charAt(left) != s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+}
