4287256d6e5caca7c1afb48d9595ec2008791086
bryancsinformatica <bryancsinformatica@gmail.com>
2024-02-06 17:40:20 -0600

    fix an if that i did not like

--- threeSum/Solution.java
+++ threeSum/Solution.java
@@ -12,28 +12,27 @@ public class Solution {
             System.out.println(l);
         }
     }
-    //Runtime: 32ms | Memory: 51.14MB 
+    //Runtime: 31ms | Memory: 51.14MB 
     public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> listSum = new ArrayList<>();
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            int j = i + 1;
-            int k = nums.length - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == 0) {
-                    listSum.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    while (j < k && nums[j] == nums[j + 1])j++;
-                    while (j < k && nums[k] == nums[k - 1])k--;
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    j++;
-                } else {
-                    k--;
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                int j = i + 1;
+                int k = nums.length - 1;
+                while (j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum == 0) {
+                        listSum.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        while (j < k && nums[j] == nums[j + 1])j++;
+                        while (j < k && nums[k] == nums[k - 1])k--;
+                        j++;
+                        k--;
+                    } else if (sum < 0) {
+                        j++;
+                    } else {
+                        k--;
+                    }
                 }
             }
         }
